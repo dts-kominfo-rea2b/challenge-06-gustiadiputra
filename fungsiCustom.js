31a1064e01aa2e640475f4ff557253ec057e5003
Gusti Adi Putra <gustiadi.putra20@gmail.com>
2022-06-13 10:34:04 +0700

    tugas

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,6 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require('fs')
+
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +20,27 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (arg) => {
+
+  const result = [];
+  fs.readFile(file1, 'utf8', function read(err, data) {
+    let content = JSON.parse(data)
+    message = content.message;
+    result.push(message.split(' ')[1])
+    fs.readFile(file2, 'utf8', function read(err, data) {
+      content = JSON.parse(data)
+      message = content[0].message;
+      result.push(message.split(' ')[1])
+      fs.readFile(file3, 'utf8', function read(err, data) {
+        content = JSON.parse(data)
+        message = content[0].data.message;
+        result.push(message.split(' ')[1])
+        console.log(result)
+        arg(err, result)
+      })
+    })
+  })
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
